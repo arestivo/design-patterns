5da794a379c7be6b8dbdd0b7fd54c904891b1a68
Andre Restivo <andre.restivo@gmail.com>
2020-03-31 18:40:56 +0100

    Changed method order in State

--- after/state/src/main/java/State.java
+++ after/state/src/main/java/State.java
@@ -1,9 +1,9 @@
 public abstract class State {
-    protected AlarmClock alarmClock;
-
     public State(AlarmClock alarmClock) {
         this.alarmClock = alarmClock;
     }
+
+    protected AlarmClock alarmClock;
 
     public abstract void buttonPressed(AlarmClock.BUTTON button);
     public abstract void buttonReleased(AlarmClock.BUTTON button);
